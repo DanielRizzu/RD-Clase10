bb1a08ea5e758f6bcb44f99876ed7d4f507f197d
DanielRizzu <96449648+DanielRizzu@users.noreply.github.com>
2023-05-21 19:26:29 -0300

    primer commit
    Ejercicio JS

---
+++ Clase10.js
@@ -0,0 +1,17 @@
+let nombre = "Daniel";
+let apellido = "Rizzuti";
+
+console.log(nombre + " " + apellido);
+
+let num1 = 2;
+let num2 = 3;
+
+let suma = num1 + num2;
+
+console.log("El resultado de la suma de " + num1 + " + " + num2 + " es: " + suma);
+
+let num3 = 10
+let suma2 = (suma + num3);
+
+console.log("Al resultado de suma le sumamos " + num3 + " y da como resultado: " + suma2);
+
